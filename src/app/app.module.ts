038d8c3a44394e73dde76f01c685412c8395e305
ziluiz <luiz021205@gmail.com>
2023-04-04 09:41:09 -0300

    nsei

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalcularComponent } from './calcular/calcular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcularComponent
   ],
   imports: [
     BrowserModule,
